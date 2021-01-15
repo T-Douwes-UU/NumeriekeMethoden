a18ee86b107f535254c80f4e8ee3680db58789a5
T-Douwes-UU <t.douwes@students.uu.nl>
2021-01-15 22:33:35 +0100

    Add global variables, add axis labels

--- Project 3/heat_diffusion.py
+++ Project 3/heat_diffusion.py
@@ -5,24 +5,30 @@ from scipy.special import erfc
 import matplotlib.pyplot as plt
 # from matplotlib.animation import FuncAnimation
 
+LENGTH = 5  # Length to plot, actual length is infinite
+T_0 = 0
+T_1 = 1
+KAPPA = 1
 
-def analytical(x: np.ndarray, t: float, temp_0=0, temp_1=1, kappa=1):
+
+def analytical(x: np.ndarray, t: float, temp_0, temp_1, kappa):
     return temp_0 + (temp_1 - temp_0) * erfc(x / 2 / np.sqrt(kappa * t))
 
 
-def animate():
-    x = np.linspace(0, 5, 100)
+def animate(length=LENGTH, temp_0=T_0, temp_1=T_1, kappa=KAPPA):
+    x = np.linspace(0, length, 100)
     t = np.arange(0.01, 2, 0.01)
 
     fig = plt.figure()
-    ax = plt.axes(xlim=(0, 5), ylim=(0, 1))
+    ax = plt.axes(xlim=(0, length), ylim=(temp_0, temp_1))
     line, = ax.plot([], [])
-    ax.set_title("Heat diffusion in a rod")
+    ax.set_title("Heat diffusion in a half-infinite rod")
+    ax.set_xlabel("$x$ (m)")
+    ax.set_ylabel("$T$ (${^\circ}$C)")
 
-    # animation function.  This is called sequentially
     def update(i):
-        y = analytical(x, t[i])
-        line.set_data(x, y)
+        y = analytical(x, t[i], temp_0, temp_1, kappa)  # Calculate the analytical temperature
+        line.set_data(x, y)  # Update the plot
         return line,
 
     return sources.Player(fig, update, frames=len(t), interval=20)
