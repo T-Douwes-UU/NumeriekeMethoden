5b8821d8b5809fc69193a149dae4dc76c35e9b6e
snibbelgaurd <gerwout.lamberink@gmail.com>
2021-01-26 11:58:26 +0100

    Update heat_diffusion.py

--- Project 3/heat_diffusion.py
+++ Project 3/heat_diffusion.py
@@ -1,37 +1,72 @@
+
 """Task 1: Heat diffusion"""
 import sources
+from sources import *
 import numpy as np
 from scipy.special import erfc
 import matplotlib.pyplot as plt
 # from matplotlib.animation import FuncAnimation
 
-LENGTH = 5  # Length to plot, actual length is infinite
-T_0 = 0
-T_1 = 1
-KAPPA = 1
+LENGTH = 5  # Length to plot (in metres), actual length is infinite
+DX = 0.001 # (metres)
+DT = 0.01 # (seconds)
+T = np.arange(DT, 2 + DT, DT) # NumPy array containing all discrete time steps
+X = np.arange(0, LENGTH + DX, DX) #NumPy array containing all discrete space steps
+TEMP_0 = 0 # Initial temperature of the rod
+TEMP_1 = 1 # The temperature at which the end of the rod is held at.
+TEMP = np.full(len(X), TEMP_0, float)
+TEMP[0] = TEMP_1
+print(TEMP)
+KAPPA = 1. #The thermal diffusion constant
 
 
 def analytical(x: np.ndarray, t, temp_0, temp_1, kappa):
     """Analytical solution for heat diffusion in a semi-infinite rod.
-
     Args:
         x: An array of distances x from the endpoint of the rod.
         t: The current time.
         temp_0: The initial temperature of the rod.
         temp_1: The temperature the end of the rod is held at.
         kappa: The thermal diffusion constant.
-
     Returns:
         A NumPy array of current temperatures along the x axis.
     """
     return temp_0 + (temp_1 - temp_0) * erfc(x / 2 / np.sqrt(kappa * t))
 
 
-def animate(length=LENGTH, temp_0=T_0, temp_1=T_1, kappa=KAPPA):
-    """Returns a FuncAnimation object."""
-    x = np.linspace(0, length, 100)
-    t = np.arange(0.01, 2, 0.01)
 
+
+
+def derivative(temp=TEMP, dt=DT, kappa=KAPPA):
+    """
+    Args:
+        Current temperature at given time
+    Return:
+        A NumPy array of the double derivative of temperature to space 
+    """
+    new_var = (temp[2:] - 2 * temp[1:-1] + temp[:-2])/dt**2
+    temp[1:-1] += new_var * KAPPA * dt
+    temp[-1] = temp[-2]
+    return temp
+    
+
+def produce_data(temp=TEMP, derivative=derivative, dt=DT, t=T):
+    print("Working...")
+    temp_list = np.zeros(,dtype=float)
+    for i in T:
+        new_temp = euler(temp, dt, derivative)
+        temp = np.concatenate(temp, new_temp)
+    print("Finished creating data array.")
+    return temp
+
+final_temp = produce_data()
+
+
+
+
+
+def animate(length=LENGTH, temp_0=TEMP_0, temp_1=TEMP_1, kappa=KAPPA):
+    """Returns a FuncAnimation object."""
     fig = plt.figure()
     ax = plt.axes(xlim=(0, length), ylim=(temp_0, temp_1))
     line, = ax.plot([], [])
@@ -43,9 +78,12 @@ def animate(length=LENGTH, temp_0=T_0, temp_1=T_1, kappa=KAPPA):
         y = analytical(x, t[i], temp_0, temp_1, kappa)  # Calculate the analytical temperature
         line.set_data(x, y)  # Update the plot
         return line,
-
-    return sources.Player(fig, update, frames=len(t), interval=20)
+    
+    return sources.Player(fig, update, frames=len(T), interval=20)
 
 
 if __name__ == '__main__':
     anim = animate()
+    
+    
+    
