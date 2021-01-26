21969fca058c2178a0cffaf735537ddb369e14be
snibbelgaurd <gerwout.lamberink@gmail.com>
2021-01-26 13:55:57 +0100

    Update heat_diffusion.py

--- Project 3/heat_diffusion.py
+++ Project 3/heat_diffusion.py
@@ -8,16 +8,15 @@ import matplotlib.pyplot as plt
 # from matplotlib.animation import FuncAnimation
 
 LENGTH = 5  # Length to plot (in metres), actual length is infinite
-DX = 0.1  # (metres)
-DT = 0.001  # (seconds)
-T = np.arange(DT, 2 + DT, DT)  # NumPy array containing all discrete time steps
-X = np.arange(0, LENGTH + DX, DX)  # NumPy array containing all discrete space steps
-TEMP_0 = 0  # Initial temperature of the rod
-TEMP_1 = 1  # The temperature at which the end of the rod is held at.
+DX = 0.1 # (metres)
+DT = 0.001 # (seconds)
+T = np.arange(DT, 2 + DT, DT) # NumPy array containing all discrete time steps
+X = np.arange(0, LENGTH + DX, DX) #NumPy array containing all discrete space steps
+TEMP_0 = 0 # Initial temperature of the rod
+TEMP_1 = 1 # The temperature at which the end of the rod is held at.
 TEMP = np.full(len(X), TEMP_0, float)
 TEMP[0] = TEMP_1
-print(TEMP)
-KAPPA = 1.  # The thermal diffusion constant
+KAPPA = 1. #The thermal diffusion constant
 
 
 def analytical(x: np.ndarray, t, temp_0, temp_1, kappa):
@@ -34,18 +33,34 @@ def analytical(x: np.ndarray, t, temp_0, temp_1, kappa):
     return temp_0 + (temp_1 - temp_0) * erfc(x / 2 / np.sqrt(kappa * t))
 
 
+
+
+
 def derivative(temp, dt=DT, dx=DX, kappa=KAPPA):
     """
     Args:
-        Current temperature at given time
+        temp: Current temperature at every spacial grid point.
+        dt: Timestep size
+        dx: Spacial step size
+        kappa: The thermal diffusion constant.
     Return:
         A NumPy array of the double derivative of temperature to space 
     """
     d_temp = kappa * (temp[2:] - 2 * temp[1:-1] + temp[:-2]) / dx**2
     return np.pad(d_temp, 1, constant_values=(0, d_temp[-1]))
+    
 
-
-def produce_data(temp=TEMP, derivative=derivative, dt=DT, t=T):
+def euler_data(temp=TEMP, derivative=derivative, dt=DT, t=T):
+    """
+    Args:
+        temp: Current temperature at every spacial grid point.
+        derivative: Double spacial derivative of the temperature.
+        dt: Timestep size.
+        t: Array containing all time steps.
+    Return:
+        A NumPy array containing the temperatures at every x step and every t step
+        found using the Euler method.
+    """
     print("Working...")
     temp_list = np.zeros((len(t), len(temp)), dtype=float)
     for i in range(len(t)):
@@ -56,15 +71,16 @@ def produce_data(temp=TEMP, derivative=derivative, dt=DT, t=T):
     return temp_list
 
 
-final_temp = produce_data()
+TEMP_euler = euler_data()
 
 
 def animate(x=X, t=T, length=LENGTH, temp_0=TEMP_0, temp_1=TEMP_1, kappa=KAPPA):
     """Returns a FuncAnimation object."""
     fig = plt.figure()
     ax = plt.axes(xlim=(0, length), ylim=(temp_0, temp_1))
-    anlytc, = ax.plot([], [])
-    eul, = ax.plot([], [])
+    anlytc, = ax.plot([], [], label='anlytical result')
+    eul, = ax.plot([], [], label='euler method')
+    plt.legend()
     ax.set_title("Heat diffusion in a half-infinite rod")
     ax.set_xlabel("$x$ (m)")
     ax.set_ylabel("$T$ (${^\circ}$C)")
@@ -72,7 +88,7 @@ def animate(x=X, t=T, length=LENGTH, temp_0=TEMP_0, temp_1=TEMP_1, kappa=KAPPA):
     def update(i):
         y = analytical(x, t[i], temp_0, temp_1, kappa)  # Calculate the analytical temperature
         anlytc.set_data(x, y)  # Update the plot
-        eul.set_data(x, final_temp[i])
+        eul.set_data(x, TEMP_euler[i])
         return anlytc,
     
     return sources.Player(fig, update, frames=len(T), interval=20)
