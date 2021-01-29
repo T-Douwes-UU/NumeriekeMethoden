6716dbca8d86e6995939fc84b9ae53d62f98afbc
T-Douwes-UU <t.douwes@students.uu.nl>
2021-01-29 14:29:20 +0100

    Add stub file shallow_water.py

---
+++ Project 3/shallow_water.py
@@ -0,0 +1,12 @@
+"""Optional task 3: The Shallow Water Equations"""
+from sources import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def derivative():
+    pass
+
+
+if __name__ == '__main__':
+    pass
