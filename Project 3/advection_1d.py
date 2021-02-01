2c52b6f410ee7e28cf9a85aaa11fdb5f1d0e8d3d
snibbelgaurd <gerwout.lamberink@gmail.com>
2021-02-01 23:55:32 +0100

    Update advection_1d.py

--- Project 3/advection_1d.py
+++ Project 3/advection_1d.py
@@ -1,5 +1,6 @@
 """Task 2: Advection in 1D"""
 import sources
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -11,7 +12,8 @@ DT = 0.001
 X = np.arange(0, LENGTH + DX, DX)
 T = np.arange(DT, 10+DT, DT)
 CONST = 0.5  # traveling speed of variations in u.
-
+now = datetime.now()  # Get the current date and time
+timestamp = now.strftime("%Y-%m-%d_%H.%M.%S")
 
 def gaussian(x_t, half_length):
     """Analytical single gaussian solution for advection in one dimension.
@@ -92,15 +94,15 @@ def numerical_data(method, state, derivative=u_derivative, dt=DT, t=T, dx=DX, co
     """Constructs a 2D array containing numerically obtained data using a specified method.
 
     Args:
-        state: Current temperature at every spacial grid point.
-        derivative: Double spacial derivative of the temperature.
+        state: Current state of u at every spacial grid point.
+        derivative: spacial derivative of u.
         method: Chosen numerical method.
         dt: Time step size.
         t: Array containing all time steps.
         dx: Spatial step size.
         const: traveling speed of variations in u.
     Returns:
-        A 2D NumPy array containing the temperatures at every x step and every t step
+        A 2D NumPy array containing the state of u at every x step and every t step
         found using the chosen method method.
     """
     print("Working...\r", end='')
@@ -145,9 +147,52 @@ DATA_MOLEN_LEAP = numerical_data(sources.leap_frog, DATA_MOLEN_ANLYTC[0])
 DATA_MOLEN_ADAMS = numerical_data(sources.adams_bashforth, DATA_MOLEN_ANLYTC[0])
 DATA_MOLEN_CRANK = numerical_data(crank_nicolson, DATA_MOLEN_ANLYTC[0])
 
+# Calculate the <R^2> between the numerical methods and the Gaussian solution.
+DEV_GAUSS_EULER = np.mean((DATA_GAUSS_EULER - DATA_GAUSS_ANLYTC)**2, axis=1)
+DEV_GAUSS_RK = np.mean((DATA_GAUSS_RK - DATA_GAUSS_ANLYTC)**2, axis=1)
+DEV_GAUSS_LEAP = np.mean((DATA_GAUSS_LEAP - DATA_GAUSS_ANLYTC)**2, axis=1)
+DEV_GAUSS_ADAMS = np.mean((DATA_GAUSS_ADAMS - DATA_GAUSS_ANLYTC)**2, axis=1)
+DEV_GAUSS_CRANK = np.mean((DATA_GAUSS_CRANK - DATA_GAUSS_ANLYTC)**2, axis=1)
+
+# Calculate the <R^2> between the numerical methods and the Gaussian solution.
+DEV_MOLEN_EULER = np.mean((DATA_MOLEN_EULER - DATA_MOLEN_ANLYTC)**2, axis=1)
+DEV_MOLEN_RK = np.mean((DATA_MOLEN_RK - DATA_MOLEN_ANLYTC)**2, axis=1)
+DEV_MOLEN_LEAP = np.mean((DATA_MOLEN_LEAP - DATA_MOLEN_ANLYTC)**2, axis=1)
+DEV_MOLEN_ADAMS = np.mean((DATA_MOLEN_ADAMS - DATA_MOLEN_ANLYTC)**2, axis=1)
+DEV_MOLEN_CRANK = np.mean((DATA_MOLEN_CRANK - DATA_MOLEN_ANLYTC)**2, axis=1)
 
 
-def animate(length=LENGTH, width=WIDTH, x=X, t=T, const=CONST):
+def deviation(x=X, t=T):
+    plt.figure()
+    plt.yscale('log')
+    plt.title(f"deviation between numerical and the Gaussian solution $<R^{2}>$")
+    plt.xlabel("$t$ (s)")
+    plt.ylabel("$<R^{2}>$")
+    plt.plot(t, DEV_GAUSS_EULER, label='Euler method')
+    plt.plot(t, DEV_GAUSS_RK, label='Runge-Kutta method')
+    plt.plot(t, DEV_GAUSS_LEAP, label='Leap-frog method')
+    plt.plot(t, DEV_GAUSS_ADAMS, label='Adams-Bashforth method')
+    plt.plot(t, DEV_GAUSS_CRANK, label='Crank-Nicolson method')
+    plt.legend()
+    plt.savefig(f"deviation task2 Gaussian {timestamp}.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
+    plt.figure()
+    plt.yscale('log')
+    plt.title(f"deviation between numerical and the Molenkamp solution $<R^{2}>$")
+    plt.xlabel("$t$ (s)")
+    plt.ylabel("$<R^{2}>$")
+    plt.plot(t, DEV_MOLEN_EULER, label='Euler method', linestyle='--')
+    plt.plot(t, DEV_MOLEN_RK, label='Runge-Kutta method')
+    plt.plot(t, DEV_MOLEN_LEAP, label='Leap-frog method')
+    plt.plot(t, DEV_MOLEN_ADAMS, label='Adams-Bashforth method')
+    plt.plot(t, DEV_MOLEN_CRANK, label='Crank-Nicolson method')    
+    plt.legend()
+    plt.savefig(f"deviation task2 Molenkamp {timestamp}.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
+
+def animate(length=LENGTH, width=WIDTH, x=X, t=T, const=CONST, beginframe=0):
     """Returns a FuncAnimation object."""
     #  x = np.arange(0, length, 0.01)
     #  t = np.arange(0.01, 10, 0.02)
@@ -159,17 +204,17 @@ def animate(length=LENGTH, width=WIDTH, x=X, t=T, const=CONST):
     gauss_rk, = ax.plot([], [], label='Gaussian solution with Runge-Kutta')
     gauss_leap, = ax.plot([], [], label='Gaussian solution with Leap-frog')
     gauss_adams, = ax.plot([], [], label='Gaussian solution with Adams-Bashforth')
-    gauss_crank, = ax.plot([], [], label='Gaussian solution with Crank-Nicolson')
-    gauss, = ax.plot([], [], linestyle='--', label='Analytical Gaussian solution')
+    gauss_crank, = ax.plot([], [], c='violet', label='Gaussian solution with Crank-Nicolson')
+    gauss, = ax.plot([], [], c='lime', linestyle=(0, (5, 5)), label='Analytical Gaussian solution')
 
-    molen_euler, = ax.plot([], [], label='Molenkamp solution with Euler')
-    molen_rk, = ax.plot([], [], label='Molenkamp solution with Runge-Kutta')
-    molen_leap, = ax.plot([], [], label='Molenkamp solution with Leap-frog')
-    molen_adams, = ax.plot([], [], label='Molenkamp solution with Adams-Bashforth')
-    molen_crank, = ax.plot([], [], label='Molenkamp solution with Crank-Nicolson')
-    molen, = ax.plot([], [], linestyle='--', label='Analytical Molenkamp solution')
+    #molen_euler, = ax.plot([], [], label='Molenkamp solution with Euler')
+    #molen_rk, = ax.plot([], [], label='Molenkamp solution with Runge-Kutta')
+    #molen_leap, = ax.plot([], [], label='Molenkamp solution with Leap-frog')
+    #molen_adams, = ax.plot([], [], label='Molenkamp solution with Adams-Bashforth')
+    #molen_crank, = ax.plot([], [], c='violet', label='Molenkamp solution with Crank-Nicolson')
+    #molen, = ax.plot([], [], c='lime', linestyle=(0, (5, 5)), label='Analytical Molenkamp solution')
 
-    plt.legend()
+    plt.legend(loc='upper left', fontsize='small')
     ax.set_title("Advection in one dimension")
     ax.set_xlabel("$x$ (m)")
     ax.set_ylabel("$u$")
@@ -186,18 +231,25 @@ def animate(length=LENGTH, width=WIDTH, x=X, t=T, const=CONST):
         gauss_adams.set_data(x, DATA_GAUSS_ADAMS[i])
         gauss_crank.set_data(x, DATA_GAUSS_CRANK[i])
 
-        molen.set_data(x, molenkamp(x_t, width))
-        molen_euler.set_data(x, DATA_MOLEN_EULER[i])
-        molen_rk.set_data(x, DATA_MOLEN_RK[i])
-        molen_leap.set_data(x, DATA_MOLEN_LEAP[i])
-        molen_adams.set_data(x, DATA_MOLEN_ADAMS[i])
-        molen_crank.set_data(x, DATA_MOLEN_CRANK[i])
+        #molen.set_data(x, molenkamp(x_t, width))
+        #molen_euler.set_data(x, DATA_MOLEN_EULER[i])
+        #molen_rk.set_data(x, DATA_MOLEN_RK[i])
+        #molen_leap.set_data(x, DATA_MOLEN_LEAP[i])
+        #molen_adams.set_data(x, DATA_MOLEN_ADAMS[i])
+        #molen_crank.set_data(x, DATA_MOLEN_CRANK[i])
+        
+        #if you want to make a screenshot of a specific frame you can uncomment the following lines:
+        #if i == 3000: #specify here what frame you would like to save.
+        #    fig.savefig(f'plot task2 frame{i} {timestamp}.png', dpi=300, bbox_inches='tight')
 
-        return gauss, gauss_euler, gauss_rk, gauss_leap, gauss_adams, gauss_crank,\
+        return gauss, gauss_euler, gauss_rk, gauss_leap, gauss_adams, gauss_crank, \
             molen, molen_euler, molen_rk, molen_leap, molen_adams, molen_crank
 
-    return FuncAnimation(fig, update, frames=len(t), interval=20, blit=True)
+    #return sources.Player(fig, update, frames=len(t), interval=20) #Interactive animation for finding which frame you want.
+    return FuncAnimation(fig, update, frames=range(beginframe,len(t)), interval=20, blit=True)
 
 
 if __name__ == '__main__':
     anim = animate()
+
+deviation()
